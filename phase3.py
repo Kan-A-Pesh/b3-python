36e87abe20bcc03ef3809f8b7d949a9a94b8713c
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-10-23 09:24:14 +0200

    feat: phase 3

---
+++ phase3.py
@@ -0,0 +1,44 @@
+import pathlib
+import requests
+from phase2 import get_category_products_data, save_to_csv
+
+CATEGORY_URL = (
+    "https://books.toscrape.com/catalogue/category/books/classics_6/index.html"
+)
+
+
+def clear_image_folder():
+    # Remove images/ folder if it exists
+    if pathlib.Path("images").exists():
+        for file in pathlib.Path("images").iterdir():
+            file.unlink()
+        pathlib.Path("images").rmdir()
+
+    # Create images/ folder
+    pathlib.Path("images").mkdir(parents=True, exist_ok=True)
+
+
+def download_images(product_data):
+    # Convert title to "Sentence Case" to "underline_case"
+    title = product_data["title"].replace(" ", "_").lower()
+
+    # Remove all characters that are not alphanumeric
+    title = "".join(char for char in title if (char.isalnum() or char == "_"))
+
+    # Download image
+    image_url = product_data["image_url"]
+    image_response = requests.get(image_url)
+    image_content = image_response.content
+    image_path = pathlib.Path("images", f"{title}.jpg")
+    image_path.write_bytes(image_content)
+
+    return product_data
+
+
+if __name__ == "__main__":
+    clear_image_folder()
+    products_data = get_category_products_data(CATEGORY_URL)
+    for product in products_data:
+        download_images(product)
+    save_to_csv(products_data)
+    print("Data extracted and saved to product_data.csv")
